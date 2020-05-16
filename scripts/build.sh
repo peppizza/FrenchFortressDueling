cb649ecb07a02714c219b08a07f34614e9b440d6
peppizza <spencervess@gmail.com>
2020-05-15 23:50:36 -0400

    added download for sourcemod

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 cd ..
+
+if [ ! -d sourcemod ]
+then
+	wget https://sm.alliedmods.net/smdrop/1.10/sourcemod-1.10.0-git6488-linux.tar.gz
+	tar zxf sourcemod-1.10.0-git6488-linux.tar.gz
+	mv addons/sourcemod .
+	rm -r cfg
+	rm -r addons
+	rm sourcemod-1.10.0-get6488-linux.tar.gz
+fi
+
 cp src/addons/sourcemod/plugins/tf2csgo.sp sourcemod/scripting/
 sourcemod/scripting/./compile.sh tf2csgo.sp
 mkdir -p build/sourcemod/plugins
